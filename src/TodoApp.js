ef55d0d3c619dfbf2f6cc0575831fe5bf1dede1d
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-05-01 22:38:34 +0300

    Add README, Fix navigation defaults

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -12,6 +12,7 @@ function TodoApp() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/index.html" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
